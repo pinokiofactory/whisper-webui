e5554b736a0d1940124e09ce1ced6428c6321237
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-04 09:44:17 -0400

    update

--- update.js
+++ update.js
@@ -1,13 +1,13 @@
 module.exports = {
   run: [{
-    method: "shell.run",
-    params: {
-      message: "git pull"
-    }
-  }, {
     method: "fs.rm",
     params: {
       path: "app/configs/default_parameters.yaml"
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      message: "git pull"
     }
   }, {
     method: "shell.run",
