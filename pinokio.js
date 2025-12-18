26585af0bbc3842126d08d417edce5b7460df7a2
6Morpheus6 <wanton-decay@gmx.net>
2025-12-18 22:08:14 +0100

    update

--- pinokio.js
+++ pinokio.js
@@ -86,7 +86,6 @@ module.exports = {
           text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
           href: "reset.js",
           confirm: "Are you sure you wish to reset the app?"
-
         }]
       }
     } else {
